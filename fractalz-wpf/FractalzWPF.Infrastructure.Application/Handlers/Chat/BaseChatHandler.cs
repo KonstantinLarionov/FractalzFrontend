79dd7a09e4a19ffa943174a52cb8d07edf292c2c
Константин Ларионов <larionov@scancity.ru>
2022-02-11 16:49:38 +0500

    minimal fix about handlers

--- fractalz-wpf/FractalzWPF.Infrastructure.Application/Handlers/Chat/BaseChatHandler.cs
+++ fractalz-wpf/FractalzWPF.Infrastructure.Application/Handlers/Chat/BaseChatHandler.cs
@@ -1,14 +1,19 @@
-﻿using FractalzWPF.Infrastructure.Application.Application;
+﻿using FractalzWPF.Application.Domains.Entities.Profile;
+using FractalzWPF.Infrastructure.Application.Application;
 using FractalzWPF.Infrastructure.Application.Domains.Enums;
+using Microsoft.Extensions.Options;
 
 namespace FractalzWPF.Infrastructure.Application.Handlers.Chat
 {
     public class BaseChatHandler
     {
-        protected readonly IConnector _connector;
-        public BaseChatHandler(IFactoryConnector connectors)
+        protected readonly IConnector _connector;     
+        protected readonly UserData MyInfo;
+
+        public BaseChatHandler(IFactoryConnector connectors, IOptions<UserData> data)
         {
             _connector = connectors.Get(ConnectorType.Chat);
+            MyInfo = data.Value;
         }
 
     }
