47b02ab435fab6c93f649476dc359ce67783130f
Константин Ларионов <kostya12277@yandex.ru>
2022-03-02 21:19:15 +0500

    ~~~

--- fractalz-wpf/FractalzWPF.Vizualizer/Elements/ServerElement.xaml.cs
+++ fractalz-wpf/FractalzWPF.Vizualizer/Elements/ServerElement.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace FractalzWPF.Infrastructure.Vizualizer.Elements
 {
@@ -8,11 +9,44 @@ namespace FractalzWPF.Infrastructure.Vizualizer.Elements
         public ServerElement()
         {
             InitializeComponent();
+            for (int i = 1; i < 10; i++)
+            {
+                treeViewRooms.Items.Add(CreateUIRoom(i, "RandomName" + i));
+            }
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             tree.Visibility = tree.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+
+        public ContextMenu CreateMenuRoom(string id)
+        {
+            var cm = new ContextMenu();
+            var mi = new MenuItem();
+            mi.Header = "Подключиться";
+            mi.Tag = id;
+            mi.Click += MiOnMouseDown;
+            cm.Items.Add(mi);
+            return cm;
+        }
+
+        private void MiOnMouseDown(object sender, RoutedEventArgs e)
+        {
+            var mi = (MenuItem) sender;
+            MessageBox.Show(mi.Tag.ToString());
+        }
+
+        public TreeViewItem CreateUIRoom(int id, string name)
+        {
+            var room = new TreeViewItem()
+            {
+                Tag = id.ToString(),
+                Header = name,
+                ContextMenu = CreateMenuRoom(id.ToString())
+            };
+            return room;
         }
     }
 }
