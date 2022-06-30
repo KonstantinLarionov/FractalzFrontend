212b0bdd2077abb0bf398dfc8a7919b516252d0d
Даниил Яковлев <daniil46@yandex.ru>
2022-06-30 12:28:31 +0500

    ~~

--- fractalz-electron/src/renderer/components/pages/TimetablePage.vue
+++ fractalz-electron/src/renderer/components/pages/TimetablePage.vue
@@ -1,16 +1,12 @@
 <template>
-  <vue-cal class=""
-           :disable-views="['years', 'year']"
+  <vue-cal class="" style="height: 100%"
+           :disable-views="['years', 'year']" active-view="week" :time-step="30"
            locale="ru"
-           active-view="month"
-           style="height: 100%"
-           :time-step="30"
-           :events="events">
-    <template v-slot:split-label="{ split, view }">
-      <i class="icon material-icons">person</i>
-      <strong :style="`color: ${split.color}`">
-        {{ split.label }}</strong>
-    </template>
+           :special-hours="specialHours"
+           :split-days="splitDays"
+           :sticky-split-labels="stickySplitLabels"
+           :events="events"
+           >
    </vue-cal>
 </template>
 
@@ -18,31 +14,54 @@
 import VueCal from 'vue-cal'
 import 'vue-cal/dist/i18n/ru.js'
 import 'vue-cal/dist/vuecal.css'
+const dailyHours = { from: 9 * 60, to: 18 * 60, class: 'business-hours', split: 1 }
 export default {
   name: "TimetablePage",
   components:{VueCal},
   data:()=>({
+    stickySplitLabels: false,
+    splitDays: [
+      // The id property is added automatically if none (starting from 1), but you can set a custom one.
+      // If you need to toggle the splits, you must set the id explicitly.
+      { id: 1, class: 'mom', label: 'Mom' },
+      { id: 2, class: 'dad', label: 'Dad'},
+    ],
+    specialHours: {
+      1: dailyHours,
+      2: dailyHours,
+      3: [
+        { from: 9 * 60, to: 12 * 60, class: 'business-hours' },
+        { from: 14 * 60, to: 18 * 60, class: 'business-hours' }
+      ],
+      4: dailyHours,
+      5: dailyHours,
+      6: dailyHours,
+      7: dailyHours,
+    },
       events: [
       {
         start: '2022-06-29 11:30',
         end: '2022-06-29 12:30',
         title: 'Need to go shopping',
         content: '<i class="v-icon material-icons">shopping_cart</i>',
-        class: 'leisure'
-      },
+        class: 'leisure',
+        split: 1
+        },
       {
         start: '2022-06-29 10:30',
         end: '2022-06-29 11:30',
         title: 'Golf with John',
         content: '<i class="v-icon material-icons">shopping_cart</i>',
-        class: 'leisure'
+        class: 'leisure',
+        split: 2
       },
       {
         start: '2022-06-28 10:30',
         end: '2022-06-28 11:30',
         title: 'Dad\'s birthday!',
         content: '<i class="v-icon material-icons">shopping_cart</i>',
-        class: 'sport'
+        class: 'sport',
+        split: 1
       }
     ]
   }),
@@ -54,20 +73,12 @@ export default {
 }
 </script>
 
-<style scoped>
-.vuecal__menu, .vuecal__cell-events-count {
-  background-color: #42b983;
-}
-.vuecal__title-bar {
-  background-color: #e4f5ef;
-}
-.vuecal__cell--today, .vuecal__cell--current {
-  background-color: rgba(240, 240, 255, 0.4);
-}
-.vuecal:not(.vuecal--day-view) .vuecal__cell--selected {
-  background-color: rgba(235, 255, 245, 0.4);
-}
-.vuecal__cell--selected:before {
-  border-color: rgba(66, 185, 131, 0.5);
+<style>
+.vuecal__cell-split.dad {background-color: rgba(221, 238, 255, 0.5);}
+.vuecal__cell-split.mom {background-color: rgba(255, 232, 251, 0.5);}
+.business-hours {
+  background-color: rgba(0, 150, 135, 0.2);
+  border: solid rgb(0, 150, 135);
+  border-width: 2px 0;
 }
 </style>
