8402f8f2400b2bf8fc2b601f3e8f0924c8f42154
Константин Ларионов <larionov@scancity.ru>
2022-11-28 11:18:58 +0500

    find user dialog

--- fractalz-electron/src/renderer/components/elements/DialogRow.vue
+++ fractalz-electron/src/renderer/components/elements/DialogRow.vue
@@ -5,7 +5,7 @@
           </div>
           <div class ="cenetmasseg">
             <div class="namean">
-              <p>Имя Фамилия</p>
+              <p>{{Name}}</p>
               <svg class="tchk" width="3" height="3" viewBox="0 0 2 2" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="1" cy="1" r="1" fill="#23BD57"/>
               </svg>
@@ -19,15 +19,11 @@
 </template>
 <script>
 import DialogElement from "../elements/DialogElement";
-import ChatPage from "./ChatPage";
 import ChatPart from "../../api/ChatPart";
 import NotifyCenter from "../../services/NotifyCenter";
 import Vue from "vue";
-import UnknownFile from "../elements/chat/filesextensions/UnknownFile";
 
 Vue.component ('dialog-element', DialogElement)
-Vue.component('unknown-file', UnknownFile)
-Vue.component ('chat-page', ChatPage)
 Vue.config.productionTip = false
 
 export default {
@@ -42,131 +38,19 @@ export default {
       notyHeader: "Диалоги Fractalz"
     }
   },
-  date() {
-    return {
-      dialogContents: [],
-    }
-  },
   props:{
+    Name:"",
     api: Object,
-    noty: Object
+    noty: Object,
+    Id:null
   },
   mounted: async function () {
-    // this.api = new ChatPart(this.$http);
-    // this.noty = new NotifyCenter();
-    // this.dialogContents = [];
-    // this.getDialogs();
-    // Vue.socketEvents.dialogsReceive = this.onDialogsUpdate;
+    this.api = new ChatPart(this.$http);
+    this.noty = new NotifyCenter();
+    Vue.socketEvents.dialogsReceive = this.onDialogsUpdate;
   },
   methods: {
-    onDialogsUpdate : function (message) {
-      var index = this.dialogContents.map(x => {
-        return x.id;
-      }).indexOf(message.id);
-      var unreadMess = this.dialogContents[index].countUnReadMessage;
-      message.countUnReadMessage = unreadMess + 1;
-      console.log(unreadMess)
-      console.log(message.countUnReadMessage)
-      this.dialogContents.splice(index, 1);
-      this.dialogContents.unshift(message);
-      this.$forceUpdate();
-      this.noty.Show({title: "Новое сообщение" , message : "DialogId: " + message.id})
-      //TODO :  + Подсветить жирным диалог который пришел
-    },
-    openChat: async function (id){
-      if (this.isFindUsers){
-        let arr = [Vue.$cookies.get('UserInfo').id, id];
-        this.dialogId = await this.createDialog(arr);
-        this.chatSelect = true;
-        return true;
-      }
-      this.dialogId = id;
-      this.chatSelect = true;
-    },
-    getDialogs: async function () {
-      var result = await this.api
-          .GetDialogs(Vue.$cookies.get('UserInfo').id)
-          .catch(response => {
-            this.noty.Show({
-              title: this.notyHeader,
-              message: "Произошла ошибка. Проверьте соединение с интернетом!"
-            });
-          });
-      if (result.data.success) {
-        var arr = [];
-        if (result.data.dialogs != null){
-          arr = result.data.dialogs.$values;
-          for (let j in arr) {
-            this.$set(this.dialogContents, j, arr[j])
-          }
-          this.$forceUpdate();
-        }
-      } else {
-        this.noty.Show({title: this.notyHeader, message: "У вас нет активных диалогов"});
-      }
-    },
-    findUsers: async function () {
-      this.isFindUsers =true;
-      var result = await this.api
-          .FindUsers(this.findStr)
-          .catch(response => {
-            this.noty.Show({
-              title: this.notyHeader,
-              message: "Произошла ошибка. Проверьте соединение с интернетом!"
-            });
-          });
-      if (result.data.success) {
-        console.log(result)
-        var arr = [];
-        if (result.data.users != null){
-          arr = result.data.users.$values;
-          this.dialogContents = [];
-          for (let j in arr) {
-            this.$set(this.dialogContents, j, arr[j])
-            console.log(this.dialogContents)
-          }
-          this.$forceUpdate();
-        }
-      }
-      else {
-        this.noty.Show({title: this.notyHeader, message: "Пользователь которого вы ищите не найден"});
-      }
-    },
-    createDialog: async function (usersId) {
-      console.log(usersId)
-      var result = await this.api
-          .CreateDialog(usersId)
-          .catch(response => {
-            this.noty.Show({
-              title: this.notyHeader,
-              message: "Произошла ошибка. Проверьте соединение с интернетом!"
-            });
-          });
-      if (result.data.success) {
-        console.log(result.data)
-        console.log(result.data.dialog.id)
-        return result.data.dialog.id
-      }
-      else {
-        this.noty.Show({title: this.notyHeader, message: "Не удалось создать диалог"});
-      }
-    },
-    deleteDialog: async function (dialogId) {
-      var result = await this.api
-          .DeleteDialog(dialogId)
-          .catch(response => {
-            this.noty.Show({
-              title: this.notyHeader,
-              message: "Произошла ошибка. Проверьте соединение с интернетом!"
-            });
-          });
-      if (result.data.success) {
-        console.log(result)
-      }
-      else {
-        this.noty.Show({title: this.notyHeader, message: "Не удалось удалить диалог"});
-      }
-    }
+
   }
 }
 </script>
